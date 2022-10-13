ceedda55f31a657495cb89fe9b2fea14a87a184e
Lysenko <lysenkoden@gmail.com>
2022-10-13 18:22:26 +0300

    range to-do by my logic

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo textTitle="Learn React" />
       <Todo textTitle="Learn English" />
       <Todo textTitle="Learn JS" />
+      <Todo textTitle="Learn React" />
     </div>
   );
 }
